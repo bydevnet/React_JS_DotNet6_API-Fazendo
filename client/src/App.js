c95486398d16f71b1f23b6f6ab54013c68c309ab
Byron Doria <brsdoria@gmail.com>
2026-08-21 16:45:10 -0300

    Código Comitado

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-export default function App() {
-  return (
-    <h1>Helo World</h1>
-  );
-}
+import Header from './Header';
+
+ export default function App() {
+   return (
+     <Header/>
+   );
+ }
+
+//  export default function App() {
+//    return (
+//      <h1>Helo World</h1>
+//    );
+//  }
