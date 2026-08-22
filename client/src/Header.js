ba2c7708c3bc4497200c135df72024400068bcee
Byron Doria <brsdoria@gmail.com>
2026-08-22 17:12:59 -0300

    Código Comitado

--- client/src/Header.js
+++
@@ -1,9 +0,0 @@
-import React, { Children } from 'react';
-
-export default function Header({children}){
-    return (
-        <div>
-            <h1>{children}</h1>
-        </div>
-    )
-}
---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,8 @@
+import React, { Children } from 'react';
+import './styles.css';
+
+export default function Login(){
+    return (
+            <h1>Bem Vindo</h1>
+    )
+}
