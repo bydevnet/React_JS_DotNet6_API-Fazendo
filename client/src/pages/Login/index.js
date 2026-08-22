b8d83a80e5a35e6c5245b27f687e0910f8b40411
Byron Doria <brsdoria@gmail.com>
2026-08-22 17:31:10 -0300

    Código Comitado

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,8 +1,25 @@
 import React, { Children } from 'react';
 import './styles.css';
 
+import logoImage from '../../assets/logo.svg';
+import padlock from '../../assets/padlock.png';
+
+
 export default function Login(){
     return (
-            <h1>Bem Vindo</h1>
+            <div className="logint-conatiner">
+                <section className="from">
+                    <img src={logoImage} alt="Erudio Logo"></img>
+                    <form>
+                        <h1>Access your Account</h1>
+                        <input placeholder="Username"></input>
+                        <input type="password" placeholder="Password"></input>
+                        <button type="submit">Login</button>
+                    </form>
+                </section>
+
+                <img src={padlock} alt="Login"></img>
+
+            </div>
     )
 }
