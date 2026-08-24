ab4b85f49bba63652076a666a7268028fe80992b
Byron Doria <brsdoria@gmail.com>
2026-08-24 13:30:00 -0300

    Código Comitado

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import api from '../../services/api';
 import './styles.css';
-
 import logoImage from '../../assets/logo.svg';
 import padlock from '../../assets/padlock.png';
 
 
 export default function Login(){
+
+    const [userName, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    async function login(e){
+        e.preventDefault();
+        const data = {
+            userName,
+            password
+        }
+        try{
+            const response = await api.post('api/auth/v1/signin', data);
+            localStorage.setItem('userName', userName);
+            localStorage.setItem('accessToken', response.data.accessToken);
+            localStorage.setItem('refreshToken', response.data.refreshToken);
+            navigate('/books');
+        }catch{
+            alert('Login failed! Try again!')
+        }
+    }
+
     return (
             <div className="logint-conatiner">
                 <section className="from">
                     <img src={logoImage} alt="Erudio Logo"></img>
-                    <form>
+                    <form onSubmit={login}>
                         <h1>Access your Account</h1>
-                        <input placeholder="Username"></input>
-                        <input type="password" placeholder="Password"></input>
+
+                        <input 
+                            placeholder="Username"
+                            value={userName}
+                            onChange={e => setUsername(e.target.value)}
+                        />
+
+                        <input 
+                            type="password" 
+                            placeholder="Password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
+
                         <button className="button" type="submit">Login</button>
                     </form>
                 </section>
